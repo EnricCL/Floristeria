43da90ff108a7e46cb388f62f22549f83b2328c2
EnricCL <enric.comes@gmail.com>
2020-07-15 13:05:43 +0200

    S'afegeix despedida al tancar el programa.

--- exerciciFloristeria/src/packageFloristeria/MenuFloristeria.java
+++ exerciciFloristeria/src/packageFloristeria/MenuFloristeria.java
@@ -89,6 +89,7 @@ public class MenuFloristeria {
 				
 				if(opcio.equals("5")) {
 					System.out.println("Programa finalitzat.");
+					System.out.println("Adéu!");
 					entrada.close();
 					System.exit(0);
 				}
